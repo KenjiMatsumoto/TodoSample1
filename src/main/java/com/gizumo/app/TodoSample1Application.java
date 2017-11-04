8f01e2a07918d7787d90afd10a651bb4b343dce0
Kenji Matsumoto <kenjimatsumoto1983@gmail.com>
2017-11-04 12:15:12 +0900

    first commit

---
+++ src/main/java/com/gizumo/app/TodoSample1Application.java
@@ -0,0 +1,12 @@
+package com.gizumo.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoSample1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoSample1Application.class, args);
+	}
+}
